46da2dd7d92b3727cd5beec6ed1896dbc2b881ea
Артём Осадчий <traibles@gmail.com>
2023-02-18 08:15:46 +0200

    update

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
-#region костыльне часы
+#region костыльные часы
 //bool Check = false; // Проверка условия
 //bool Delta = false;
 //char? CheckChar = null; // 
